a21252220b096653ef08f97453bef91e4c5187bc
sara-likaount <saralikaunt@gmail.com>
2022-05-15 15:30:12 +0300

    n

---
+++ main.js
@@ -0,0 +1,42 @@
+// var names= ["sara", "shlomo", "guy", "yoni" ,"nati"];
+// for (i=0; i<names.length; i++){
+//     console.log(names[i]);
+// }
+// console.log(names[2]);
+// console.log(names);
+
+// var num= [76, 85, 98 , 65, 74, 99, 3];
+// console.log(num);
+// console.log(num[3]);
+// for(i=0; i<num.length; i++){
+//     console.log(num[i]);
+// }
+
+// var numbers= [13, 15,4,9,26 ,27, 86,45,9,39];
+// for ( i=0; i< numbers.length; i++){
+//     if (numbers[i]%2==0){
+//         console.log(numbers[i]);}
+// }
+
+// var myArray= ["he", 7, "jh", 8, 9];
+// for (i=0; i<myArray.length; i++){
+//     if (myArray[i]*0!=0){
+//         console.log(myArray[i]);
+//     }
+// }
+
+// var empty=[];
+// empty.push(9);
+// console.log(empty);
+
+// var num= [];
+// num.push(0,1,2,3,4,5,6,7,8,9);
+// console.log(num);
+
+var num=[];
+for(i=0; i<10; i++){
+    num.push(i);
+    console.log(num[i]);
+    
+}
+console.log(num);
